92a92acbde1c34f8b20e76da7abff9d2da228742
George Bockari <bitwhys@gmail.com>
2024-01-01 04:35:32 -0500

    feat: Add support for @storybook/addon-themes

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,14 +1,14 @@
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export function Button({ children, ...other }: ButtonProps): JSX.Element {
   return (
-    <button type="button" {...other}>
+    <button className="text-lg" type="button" {...other}>
       {children}
     </button>
-  );
+  )
 }
 
-Button.displayName = "Button";
+Button.displayName = 'Button'
